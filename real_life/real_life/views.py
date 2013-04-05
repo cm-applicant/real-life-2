289e5d93caebc9829adbd73b4762ae7452037e89
David Zwarg <david.zwarg@gmail.com>
2013-04-04 22:16:16 -0400

    Added bootstrap HTML template.

--- real_life/real_life/views.py
+++ real_life/real_life/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-    return HttpResponse('hello, world')
+    return render_to_response('index.html')
